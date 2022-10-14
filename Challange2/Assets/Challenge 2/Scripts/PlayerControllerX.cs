bc84ec580239c80ba6751b893682f972c3d9e0e8
Anklestonn <113989718+Anklestonn@users.noreply.github.com>
2022-10-14 20:17:41 +0100

    weblg

---
+++ Challange2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float slowerFire = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && slowerFire <= 0)
+
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            slowerFire = 1;
+        }
+        if (slowerFire >= 0)
+        { 
+
+        slowerFire -= Time.deltaTime;
+        }
+    }
+}
